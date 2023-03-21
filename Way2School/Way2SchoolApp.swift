e12a2ecf02f57a7e65cc973db3c6a50ef57a0ee5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-03-21 09:14:07 +0100

    Proyecto nuevo

---
+++ Way2School/Way2SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Way2SchoolApp.swift
+//  Way2School
+//
+//  Created by Ion Jaureguialzo Sarasola on 21/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Way2SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
